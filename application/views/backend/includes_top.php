4de489258d0d81a57f3b159d77c1547d8c19e3c8
ZinabebaidZ123 <zinabebaid22@gmail.com>
2024-11-01 21:36:59 +0200

    update the global style structure

--- application/views/backend/includes_top.php
+++ application/views/backend/includes_top.php
@@ -32,6 +32,39 @@
 <link href="<?php echo base_url('assets/backend/css/dashboardstyle/home/inside.css') ?>" rel="stylesheet" type="text/css" />
 <link href="<?php echo base_url('assets/backend/css/dashboardstyle/home/navbar.css') ?>" rel="stylesheet" type="text/css" />
 <!-- end new style(Shaymaa)-->
+<link rel="shortcut icon" href="<?=base_url('uploads/system/'.get_frontend_settings('favicon'))?>">
+<!-- third party css -->
+<link href="<?=base_url('assets/backend/css/vendor/jquery-jvectormap-1.2.2.css')?>" rel="stylesheet" type="text/css" />
+<link href="<?=base_url('assets/backend/css/vendor/dataTables.bootstrap4.css')?>" rel="stylesheet" type="text/css" />
+<link href="<?=base_url('assets/backend/css/vendor/responsive.bootstrap4.css')?>" rel="stylesheet" type="text/css" />
+<link href="<?=base_url('assets/backend/css/vendor/buttons.bootstrap4.css')?>" rel="stylesheet" type="text/css" />
+<link href="<?=base_url('assets/backend/css/vendor/select.bootstrap4.css')?>" rel="stylesheet" type="text/css" />
+<link href="<?=base_url('assets/backend/css/vendor/summernote-bs4.css')?>" rel="stylesheet" type="text/css" />
+<link href="<?=base_url('assets/backend/css/vendor/fullcalendar.min.css')?>" rel="stylesheet" type="text/css" />
+<link href="<?=base_url('assets/backend/css/vendor/dropzone.css')?>" rel="stylesheet" type="text/css" />
+<!-- third party css end -->
+<!-- App css -->
+<!-- start on progress one by(Shaymaa)-->
+<link href="<?=base_url('assets/backend/css/app.min.css')?>" rel="stylesheet" type="text/css" />
+<!-- end on progress one by(Shaymaa)-->
+<link href="<?=base_url('assets/backend/css/icons.min.css')?>" rel="stylesheet" type="text/css" />
 
+<!-- start on progress tow by(Shaymaa) -->
+<link href="<?=base_url('assets/backend/css/main.css')?>" rel="stylesheet" type="text/css" />
+<!-- end on progress tow by(Shaymaa)-->
+
+<!-- font awesome 5 -->
+<link href="<?=base_url('assets/backend/css/fontawesome-all.min.css')?>" rel="stylesheet" type="text/css" />
+<link href="<?=base_url('assets/backend/css/font-awesome-icon-picker/fontawesome-iconpicker.min.css')?>" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css">
+
+<!-- start new style (Shaymaa) -->
+<link href="<?=base_url('assets/backend/css/dashboardstyle/leftside.css')?>" rel="stylesheet" type="text/css" />
+<link href="<?=base_url('assets/backend/css/dashboardstyle/inside.css')?>" rel="stylesheet" type="text/css" />
+<link href="<?=base_url('assets/backend/css/dashboardstyle/navbar.css')?>" rel="stylesheet" type="text/css" />
+<!-- end new style(Shaymaa)-->
+
+<script src="<?=base_url('assets/backend/js/jquery-3.3.1.min.js')?>" charset="utf-8"></script>
+<script src="<?=site_url('assets/backend/js/onDomChange.js')?>"></script>
 <script src="<?php echo base_url('assets/backend/js/jquery-3.3.1.min.js'); ?>" charset="utf-8"></script>
 <script src="<?php echo site_url('assets/backend/js/onDomChange.js');?>"></script>
