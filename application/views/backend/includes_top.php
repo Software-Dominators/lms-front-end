d1305af877bdd2e1b919e74f248ae3733a1751a6
ZinabebaidZ123 <zinabebaid22@gmail.com>
2024-11-02 23:38:37 +0200

    coupons-page

--- application/views/backend/includes_top.php
+++ application/views/backend/includes_top.php
@@ -52,16 +52,21 @@
 <!-- start on progress tow by(Shaymaa) -->
 <link href="<?=base_url('assets/backend/css/main.css')?>" rel="stylesheet" type="text/css" />
 <!-- end on progress tow by(Shaymaa)-->
-
+   <!-- fontawesome-link  -->
+   <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+      integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
 <!-- font awesome 5 -->
 <link href="<?=base_url('assets/backend/css/fontawesome-all.min.css')?>" rel="stylesheet" type="text/css" />
 <link href="<?=base_url('assets/backend/css/font-awesome-icon-picker/fontawesome-iconpicker.min.css')?>" rel="stylesheet" type="text/css" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css">
 
 <!-- start new style (Shaymaa) -->
-<link href="<?=base_url('assets/backend/css/dashboardstyle/leftside.css')?>" rel="stylesheet" type="text/css" />
-<link href="<?=base_url('assets/backend/css/dashboardstyle/inside.css')?>" rel="stylesheet" type="text/css" />
-<link href="<?=base_url('assets/backend/css/dashboardstyle/navbar.css')?>" rel="stylesheet" type="text/css" />
+<link href="<?=base_url('assets/backend/css/dashboardstyle/style.css')?>" rel="stylesheet" type="text/css" />
 <!-- end new style(Shaymaa)-->
 
 <script src="<?=base_url('assets/backend/js/jquery-3.3.1.min.js')?>" charset="utf-8"></script>
